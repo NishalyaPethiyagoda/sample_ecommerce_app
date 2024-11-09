3ace7a3589069ec17c69864efc63d68c3657d4d0
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-09 21:30:54 +0530

    product detail screen build

--- lib/screens/product_detail/view/product_detail_screen.dart
+++ lib/screens/product_detail/view/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sample_ecommerce_app/network/http_methods.dart';
+import 'package:sample_ecommerce_app/widgets/bottom_button.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({super.key});
@@ -8,33 +10,100 @@ class ProductDetailScreen extends StatefulWidget {
 }
 
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  final HttpMethods _httpMethods = HttpMethods();
+
+
   @override
   Widget build(BuildContext context) {
 
     double screenHeight = MediaQuery.of(context).size.height;
+    final response = _httpMethods.commonApiGet();
 
     return Scaffold(
       appBar: AppBar(
-
+        backgroundColor: Color(0xFFF8C1A5),
+        centerTitle: true,
+        title: Text(
+          "Product Details", 
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+          ),
+        // actions: [
+        //   IconButton(
+        //   onPressed: (){
+        //     // expandSearch = true;
+        //   }, 
+        //   icon: Icon(Icons.search)
+        // ),]
       ),
       body: SafeArea(
         child: Stack(
-          children: [ 
-            Container(
-             color: Colors.amber,
-             height: screenHeight * 0.6,
+          children: [
+            Column(
+              children: [
+                Container(
+                  height: screenHeight * 0.45,
+                  color: const Color(0xFFF8C1A5),
+                ),
+                Expanded(
+                  child: Container(
+                    color: const Color(0xFFFFF3EC),
+                  ),
+                ),
+              ],
             ),
-            Expanded(
+            Positioned(
+              top: 5,
+              left: 25,
+              right: 25,
               child: Container(
-                color: Colors.white,
-                child: Text("hii"),
+                decoration: BoxDecoration(
+                  color: Colors.white, 
+                  borderRadius: BorderRadius.circular(16.0), 
+                ),
+                height: screenHeight * 0.5,
+                child: Column(
+                  children: [
+                    Text("data"),
+                    Expanded(
+                      child: Text("hi")
+                    ),
+                    Text("Price - \$ 15.00"),
+                  ],
+                ),
               ),
-             
             ),
-
+            Positioned(
+              top: 5 + screenHeight * 0.5 + 20,
+              left: 25,
+              right: 25,
+              child: Container(
+                decoration: BoxDecoration(
+                  // color: Colors.white, 
+                  borderRadius: BorderRadius.circular(16.0), 
+                ),
+                height: screenHeight * 0.22,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Description", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+                    Expanded(
+                      child: Text("hi \n sdsf\nsdfsf \n sds", overflow: TextOverflow.ellipsis, maxLines: 4,)
+                    ),
+                    Text("Price - \$ 15.00"),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 5 + screenHeight * 0.5 +  screenHeight * 0.2 + 35,
+              left: 25,
+              right: 25,
+              child: AnimatedGradientButton(buttonText: "Add to Cart",)
+            ),
           ],
-        )
         ),
+      ),
     );
+
   }
 }
---
+++ lib/widgets/bottom_button.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class AnimatedGradientButton extends StatefulWidget {
+  final String buttonText;
+  const AnimatedGradientButton({super.key, required this.buttonText});
+
+  @override
+  _AnimatedGradientButtonState createState() => _AnimatedGradientButtonState();
+}
+
+class _AnimatedGradientButtonState extends State<AnimatedGradientButton> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1, milliseconds: 350),
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.repeat(); // Restart the animation to create a continuous flow
+        }
+      });
+
+    _controller.forward(); // Start the animation
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: const [
+                 Color(0xFFFFA726),
+                 Color(0xFFFFD466), 
+              ],
+              stops: [
+                _controller.value,                      
+                _controller.value + 0.2 < 1.0 
+                  ? _controller.value + 0.3 
+                  : 1.0,
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              widget.buttonText,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
