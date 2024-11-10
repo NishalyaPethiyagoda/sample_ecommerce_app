8a4dc16f1bd648bbf99a604c66bda6f8bc5ffc45
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-11 01:56:48 +0530

    navigation button disabling logic added

--- lib/navigation/main_navigation.dart
+++ lib/navigation/main_navigation.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sample_ecommerce_app/providers/cart_provider.dart';
 import 'package:sample_ecommerce_app/screens/cart/view/cart_screen.dart';
 import 'package:sample_ecommerce_app/screens/home/view/home_screen.dart';
 
@@ -16,6 +18,10 @@ class _MainNavigationState extends State<MainNavigation> {
   ];
 
   void _onItemTapped(int index) {
+    if(index == 1 && Provider.of<CartProvider>(context, listen: false).cartItemsList.isEmpty){
+        return;
+      }
+
     setState(() {
       _selectedIndex = index;
     });
@@ -29,14 +35,21 @@ class _MainNavigationState extends State<MainNavigation> {
         children: _screens,
       ),
       bottomNavigationBar: BottomNavigationBar(
+        
         backgroundColor: Color.fromARGB(255, 255, 249, 246),        
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         selectedItemColor: Color(0xFFFFA726),
         unselectedItemColor: const Color.fromARGB(255, 131, 131, 131), 
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
         ],
       ),
     );
