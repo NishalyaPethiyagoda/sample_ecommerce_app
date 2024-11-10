06d6b5975899827a38eb48b5e7b39575af2df2e0
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-11 02:47:10 +0530

    user model created

---
+++ lib/models/user_model.dart
@@ -0,0 +1,8 @@
+
+class UserModel {
+  String name;
+  String phoneNumber;
+  String shippingAddress;
+
+  UserModel({required this.name, required this.phoneNumber, required this.shippingAddress});
+}
