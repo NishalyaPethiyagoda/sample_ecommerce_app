ce197714c4e5be3a0ef25961cc93a194b2a9590a
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-11 02:47:28 +0530

    check out screen implemented

--- lib/screens/checkout/view/checkout_screen.dart
+++ lib/screens/checkout/view/checkout_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sample_ecommerce_app/models/product_model.dart';
+import 'package:sample_ecommerce_app/models/user_model.dart';
 import 'package:sample_ecommerce_app/providers/cart_provider.dart';
+import 'package:sample_ecommerce_app/widgets/bottom_button.dart';
 import 'package:sample_ecommerce_app/widgets/product_dashboard_card.dart';
 
 class CheckoutScreen extends StatefulWidget {
@@ -11,6 +14,18 @@ class CheckoutScreen extends StatefulWidget {
 }
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  UserModel user = UserModel(name: '', phoneNumber: '', shippingAddress: '');
+
+  double getTotalPrice (List<ProductModel> cartItems){
+    double totalPrice = 0.0;
+
+    for(var item in cartItems){
+      totalPrice += item.price;
+    }
+    return totalPrice;
+  }
+
   @override
   Widget build(BuildContext context) {
     final cartProvider = Provider.of<CartProvider>(context);
@@ -20,17 +35,18 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 251, 169, 128),
         centerTitle: false,
+        iconTheme: IconThemeData(color: Colors.white.withOpacity(0.85)),
         title: Text(
-          "My Cart",
+          "Checkout",
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white.withOpacity(0.85)),
         ),
-        // actions: [
-        //   IconButton(
-        //   onPressed: (){
-        //     // expandSearch = true;
-        //   },
-        //   icon: Icon(Icons.search)
-        // ),]
+        actions: [
+          IconButton(
+          onPressed: (){
+            // expandSearch = true;
+          },
+          icon: Icon(Icons.search)
+        ),]
       ),
       body: SafeArea(
         child: Container(
@@ -38,31 +54,139 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
             gradient: LinearGradient(
               colors: [
                 Color.fromARGB(255, 251, 169, 128),
-                Color.fromARGB(255, 251, 222, 208), 
+                Color.fromARGB(255, 251, 222, 208),
               ],
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
             ),
           ),
-          child: Padding(  
+          child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListView.builder(
-              itemCount: cartItems.length,
-              itemBuilder: (context, index) {
-                return Row(
-                  children: [
-                    Expanded(
-                      child: ProductDashboardCard(
-                        product: cartItems[index],
+            child: Stack(
+              children: [ 
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Form(
+                        key: _key,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'Enter your name',
+                                border: InputBorder.none,
+                                filled: true,
+                                fillColor: Colors.white.withOpacity(0.85),
+                              ),
+                              keyboardType: TextInputType.name,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your name';
+                                }
+                                return null;
+                              },
+                              onChanged: (value) {
+                                setState(() {
+                                  user.name = value;
+                                });
+                              },
+                            ),
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'Enter your address',
+                                border: InputBorder.none,
+                                filled: true,
+                                fillColor: Colors.white.withOpacity(0.85),
+                              ),
+                              keyboardType: TextInputType.streetAddress,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your address';
+                                }
+                                return null;
+                              },
+                              onChanged: (value) {
+                                setState(() {
+                                  user.shippingAddress = value;
+                                });
+                              },
+                            ),
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'Enter your phone number',
+                                border: InputBorder.none,
+                                filled: true,
+                                fillColor: Colors.white.withOpacity(0.85),
+                              ),
+                              keyboardType: TextInputType.phone,
+                              validator: (value) {
+                                if (value == null || value.isEmpty || value.length != 10) {
+                                  return 'Please enter a valid phone number';
+                                }
+                                return null;
+                              },
+                              onChanged: (value) {
+                                setState(() {
+                                  user.phoneNumber = value;
+                                });
+                              },
+                            ),
+                            const SizedBox(height: 24),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                );
-              },
+                      Column(
+                        children: [
+                          const Text(
+                            'Cart Summary',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 8),
+                          // Expanded(
+                          //   child: ListView.builder(
+                          //     itemCount: cartItems.length,
+                          //     itemBuilder: (context, index) {
+                          //       var item = cartItems[index];
+                          //       return ListTile(
+                          //         title: Text(item.name),
+                          //         // subtitle: Text('Quantity: ${item.quantity}'),
+                          //         // trailing: Text('\$${item.price * item.quantity}'),
+                          //       );
+                          //     },
+                          //   ),
+                          // ),
+                          const SizedBox(height: 16),
+                          Text(
+                            'Total: \$${getTotalPrice(cartItems).toStringAsFixed(2)}',
+                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 16),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 2,
+                  left: 25,
+                  right: 25,
+                    child: AnimatedGradientButton(
+                      buttonText: "Place Order",
+                      onPressed: (){
+                        if (_key.currentState?.validate() ?? false) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing order...')),
+                          );
+                      }} 
+                    )
+                ),
+              ]
             ),
           ),
         ),
-      )
+      ),
     );
   }
 }
