4f609bf6ebb1d2a09806a1aa2dec3e01d5ec4b12
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-10 16:32:06 +0530

    dashbord product card developed

--- lib/widgets/product_dashboard_card.dart
+++ lib/widgets/product_dashboard_card.dart
@@ -1,46 +1,55 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:sample_ecommerce_app/models/product_model.dart';
+import 'package:sample_ecommerce_app/screens/product_detail/view/product_detail_screen.dart';
 
 class ProductDashboardCard extends StatelessWidget {
-  final String productName;
-  final String productImage;
-  final double productPrice;
-  final double? rating;
-  const ProductDashboardCard({super.key, required this.productName, required this.productImage, required this.productPrice, required this.rating});
+  final ProductModel product;
+  const ProductDashboardCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.network(productImage, width: 50, height: 50),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(productName, overflow: TextOverflow.ellipsis,),
-                  Text(productPrice.toString()),
-                  rating != null ? Row(
-                  children: List.generate(
-                    5,
-                    (star) => Icon(
-                      Icons.star,
-                      color: star < rating!.toInt() ? Colors.orange : Colors.grey,
-                      size: 12,
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => ProductDetailScreen(
+            product: product,
+          )
+        ));
+      
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.network(product.imageUrl, width: 50, height: 50),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(product.name, overflow: TextOverflow.ellipsis,),
+                    Text(product.price.toString()),
+                    product.rating.rating != null ? Row(
+                    children: List.generate(
+                      5,
+                      (star) => Icon(
+                        Icons.star,
+                        color: star < product.rating.rating!.toInt() ? Colors.orange : Colors.grey,
+                        size: 12,
+                      ),
                     ),
-                  ),
-                ): Container(),
-                ],
+                  ): Container(),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
