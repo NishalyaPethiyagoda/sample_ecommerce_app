2d6149af39793e0ef9c1bdb0a124e2d482079faf
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-12 05:47:54 +0530

    text field outside focus off implemented

--- lib/screens/home/view/home_screen.dart
+++ lib/screens/home/view/home_screen.dart
@@ -54,92 +54,101 @@ class _HomeScreenState extends State<HomeScreen> {
             isLoading = false;
           }
         },
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor:  const Color.fromARGB(255, 251, 169, 128),
-            centerTitle: false,
-            title: Text(
-              "Products",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white.withOpacity(0.85)),
-            ),
-            actions: [
-              expandSearch 
-                ? Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0,0.0,12.0, 0.0),
-                  child: SizedBox(
-                    width: 200,
-                    child: TextField(
-                        controller: searchController,
-                        decoration: InputDecoration(
-                          hintText: 'Search products...',
-                          contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          filled: true,
-                          fillColor: Colors.white.withOpacity(0.85),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25.0), // Adjust the radius as needed
-                            borderSide: BorderSide.none, // Optional: to remove border color
+        child: GestureDetector(
+          onTap: (){
+            setState(() {
+              FocusScope.of(context).unfocus();
+              expandSearch = !expandSearch;
+            });
+          },
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor:  const Color.fromARGB(255, 251, 169, 128),
+              centerTitle: false,
+              title: Text(
+                "Products",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white.withOpacity(0.85)),
+              ),
+              actions: [
+                expandSearch 
+                  ? Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0,0.0,12.0, 0.0),
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(
+                          autofocus: false,
+                          controller: searchController,
+                          decoration: InputDecoration(
+                            hintText: 'Search products...',
+                            contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                            filled: true,
+                            fillColor: Colors.white.withOpacity(0.85),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0), // Adjust the radius as needed
+                              borderSide: BorderSide.none, // Optional: to remove border color
+                            ),
                           ),
                         ),
+                    ),
+                  )
+                  : IconButton(
+                      onPressed: (){
+                        setState(() {
+                          expandSearch = !expandSearch;
+                        });
+                      },
+                      icon: const Icon(Icons.search)
+                    ) 
+              ]
+            ),
+            body: Consumer<ProductProvider>(
+              builder: (context, productProvider, child) {
+                return SafeArea(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 251, 169, 128),
+                          Color.fromARGB(255, 251, 222, 208), 
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
                       ),
-                  ),
-                )
-                : IconButton(
-                    onPressed: (){
-                      setState(() {
-                        expandSearch = !expandSearch;
-                      });
-                    },
-                    icon: const Icon(Icons.search)
-                  ) 
-            ]
-          ),
-          body: Consumer<ProductProvider>(
-            builder: (context, productProvider, child) {
-              return SafeArea(
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 251, 169, 128),
-                        Color.fromARGB(255, 251, 222, 208), 
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
+                    ),
+                    child: Padding(  
+                      padding: const EdgeInsets.all(8.0),
+                      child: isLoading
+                          ? const Center(child: CircularProgressIndicator(color: Colors.grey,))
+                          : ListView.builder(
+                              itemCount: (productProvider.filteredProducts.length / 2).ceil(),
+                              itemBuilder: (context, index) {
+                                int firstIndex = index * 2;
+                                int secondIndex = firstIndex + 1;
+                    
+                                ProductModel leftProduct = productProvider.filteredProducts[firstIndex];
+                                ProductModel? rightProduct = secondIndex < productProvider.filteredProducts.length ? productProvider.filteredProducts[secondIndex] : null;
+                                return Row(
+                                  children: [
+                                    Expanded(
+                                      child: ProductDashboardCard(
+                                        product: leftProduct,
+                                        
+                                      ),
+                                    ),
+                                    rightProduct != null ? Expanded(
+                                      child: ProductDashboardCard(
+                                        product: rightProduct,
+                                      ),
+                                    ) : Container(),
+                                  ],
+                                );
+                              },
+                            ),
                     ),
                   ),
-                  child: Padding(  
-                    padding: const EdgeInsets.all(8.0),
-                    child: isLoading
-                        ? const Center(child: CircularProgressIndicator(color: Colors.grey,))
-                        : ListView.builder(
-                            itemCount: (productProvider.filteredProducts.length / 2).ceil(),
-                            itemBuilder: (context, index) {
-                              int firstIndex = index * 2;
-                              int secondIndex = firstIndex + 1;
-                  
-                              ProductModel leftProduct = productProvider.filteredProducts[firstIndex];
-                              ProductModel? rightProduct = secondIndex < productProvider.filteredProducts.length ? productProvider.filteredProducts[secondIndex] : null;
-                              return Row(
-                                children: [
-                                  Expanded(
-                                    child: ProductDashboardCard(
-                                      product: leftProduct,
-                                      
-                                    ),
-                                  ),
-                                  rightProduct != null ? Expanded(
-                                    child: ProductDashboardCard(
-                                      product: rightProduct,
-                                    ),
-                                  ) : Container(),
-                                ],
-                              );
-                            },
-                          ),
-                  ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         ),
       ),
