bf70118bfd65c183a512ee2607c8a7ef92a9ef8a
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-12 20:44:10 +0530

    text size is added

--- lib/widgets/text_gradient_container.dart
+++ lib/widgets/text_gradient_container.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 
 class TextGradientContainer extends StatelessWidget {
   final String text;
-  const TextGradientContainer({super.key, required this.text});
+  final double textSize;
+  const TextGradientContainer({super.key, required this.text, required this.textSize});
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +25,7 @@ class TextGradientContainer extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 2.0),
         child: Text(
           text, 
-          style: TextStyle(color: Colors.white.withOpacity(0.85),fontSize: 16, fontWeight: FontWeight.w600)
+          style: TextStyle(color: Colors.white.withOpacity(0.85),fontSize: textSize, fontWeight: FontWeight.w600)
         ),
       ),
     );
