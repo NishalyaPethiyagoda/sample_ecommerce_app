557aebfd40d45a7293f1460fe6229228377b657c
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-11-12 20:45:08 +0530

    condition changed

--- lib/screens/product_detail/view/product_detail_screen.dart
+++ lib/screens/product_detail/view/product_detail_screen.dart
@@ -23,169 +23,173 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
 
     double screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 251, 169, 128),
-        centerTitle: true,
-        iconTheme: const IconThemeData(
-          color: Colors.white, // Set the back button color to white
-        ),
-        title: Text(
-          widget.product.name, 
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white.withOpacity(0.85)),
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 251, 169, 128),
+            centerTitle: true,
+            iconTheme: const IconThemeData(
+              color: Colors.white, // Set the back button color to white
+            ),
+            title: Text(
+              widget.product.name, 
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white.withOpacity(0.85)),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              ),
           ),
-      ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Column(
+          body: SafeArea(
+            child: Stack(
               children: [
-                Container(
-                  height: screenHeight * 0.48,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 251, 169, 128),
-                        Color(0xFFF8C1A5), 
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
+                Column(
+                  children: [
+                    Container(
+                      height: screenHeight * 0.48,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 251, 169, 128),
+                            Color(0xFFF8C1A5), 
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: const Color(0xFFFFF3EC),
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: 5,
+                  left: 25,
+                  right: 25,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white, 
+                      borderRadius: BorderRadius.circular(16.0), 
+                    ),
+                    height: screenHeight * 0.51,
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Stack(
+                        children: [
+                          Center(child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 20.0),
+                            child: Hero(
+                              tag: widget.product.productId,
+                              child: Image.network(widget.product.imageUrl),
+                            ),
+                          )),
+                          Positioned(
+                            left: 5,
+                            bottom: 5,
+                            child: TextGradientContainer(
+                              text: "\$ ${widget.product.price.toString()}",
+                              textSize: 16,
+                            ),
+                          ),
+                          Positioned(
+                            right: 5,
+                            bottom: 5,
+                            child: widget.product.rating.rating != null 
+                              ? Row(
+                                  children: List.generate(
+                                    5,
+                                    (star) => Icon(
+                                      Icons.star,
+                                      color: star < widget.product.rating.rating!.toInt() ? Colors.orange : Colors.grey,
+                                      size: 16,
+                                    ),
+                                  ),
+                                )
+                              : Container(),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                Expanded(
+                Positioned(
+                  top: 5 + screenHeight * 0.5 + 20,
+                  left: 25,
+                  right: 25,
                   child: Container(
-                    color: const Color(0xFFFFF3EC),
+                    decoration: BoxDecoration(
+                      // color: Colors.white, 
+                      borderRadius: BorderRadius.circular(16.0), 
+                    ),
+                    height: screenHeight * 0.22,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text("Description", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+                            Text("Count - ${widget.product.rating.count}", style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600)),
+                          ],
+                        ),
+                        Expanded(
+                          child: widget.isProductAddedToCart 
+                            ? SingleChildScrollView(
+                                child: Text(
+                                  widget.product.description, 
+                                  overflow: null, 
+                                  maxLines: null,
+                              ))
+                            : Stack(
+                              children: [
+                                SingleChildScrollView(
+                                child: GestureDetector(
+                                  onTap: (){
+                                    setState((){
+                                      isNotExpanded = !isNotExpanded;
+                                    });
+                                  },
+                                  child: Text(
+                                    widget.product.description, 
+                                    overflow: isNotExpanded ? TextOverflow.ellipsis : null, 
+                                    maxLines: isNotExpanded ? 4 : null,
+                                  )
+                                  )),
+                                  isNotExpanded ? const Positioned(
+                                        right: 0, 
+                                        bottom: 3,
+                                        child: Text("Tap Description >", style: const TextStyle(fontSize: 13, fontWeight: FontWeight.w500))
+                                      )
+                                      : Container()
+                              ] 
+                            )
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
-            Positioned(
-              top: 5,
-              left: 25,
-              right: 25,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white, 
-                  borderRadius: BorderRadius.circular(16.0), 
-                ),
-                height: screenHeight * 0.51,
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Stack(
-                    children: [
-                      Center(child: Padding(
-                        padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 20.0),
-                        child: Hero(
-                          tag: widget.product.productId,
-                          child: Image.network(widget.product.imageUrl),
-                        ),
-                      )),
-                      Positioned(
-                        left: 5,
-                        bottom: 5,
-                        child: TextGradientContainer(
-                          text: "\$ ${widget.product.price.toString()}",
-                        ),
-                      ),
-                      Positioned(
-                        right: 5,
-                        bottom: 5,
-                        child: widget.product.rating.rating != null 
-                          ? Row(
-                              children: List.generate(
-                                5,
-                                (star) => Icon(
-                                  Icons.star,
-                                  color: star < widget.product.rating.rating!.toInt() ? Colors.orange : Colors.grey,
-                                  size: 16,
-                                ),
-                              ),
-                            )
-                          : Container(),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 5 + screenHeight * 0.5 + 20,
-              left: 25,
-              right: 25,
-              child: Container(
-                decoration: BoxDecoration(
-                  // color: Colors.white, 
-                  borderRadius: BorderRadius.circular(16.0), 
-                ),
-                height: screenHeight * 0.22,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text("Description", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
-                        Text("Count - ${widget.product.rating.count}", style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600)),
-                      ],
-                    ),
-                    Expanded(
-                      child: widget.isProductAddedToCart 
-                        ? SingleChildScrollView(
-                            child: Text(
-                              widget.product.description, 
-                              overflow: null, 
-                              maxLines: null,
-                          ))
-                        : Stack(
-                          children: [
-                            SingleChildScrollView(
-                            child: GestureDetector(
-                              onTap: (){
-                                setState((){
-                                  isNotExpanded = !isNotExpanded;
-                                });
-                              },
-                              child: Text(
-                                widget.product.description, 
-                                overflow: isNotExpanded ? TextOverflow.ellipsis : null, 
-                                maxLines: isNotExpanded ? 4 : null,
-                              )
-                              )),
-                              isNotExpanded ? const Positioned(
-                                    right: 0, 
-                                    bottom: 3,
-                                    child: Text("Tap Description >", style: const TextStyle(fontSize: 13, fontWeight: FontWeight.w500))
-                                  )
-                                  : Container()
-                          ] 
-                        )
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            widget.isProductAddedToCart 
-              ? Container() 
-              : Positioned(
-                bottom: 2,
-                left: 25,
-                right: 25,
-                  child: AnimatedGradientButton(
-                    buttonText: "Add to Cart",
-                    onPressed: (){
-                      Provider.of<CartProvider>(context, listen: false).addItemToCart(widget.product);
-                      Provider.of<ProductProvider>(context, listen: false).removeProduct(widget.product);
-                      Future.microtask(() => Navigator.pop(context));
-                    },
-                  ),
-                ),
-          ],
+          ),
         ),
-      ),
+        if(!widget.isProductAddedToCart)
+          Positioned(
+            bottom: 2,
+            left: 25,
+            right: 25,
+              child: AnimatedGradientButton(
+                buttonText: "Add to Cart",
+                onPressed: (){
+                  Provider.of<CartProvider>(context, listen: false).addItemToCart(widget.product);
+                  Provider.of<ProductProvider>(context, listen: false).removeProduct(widget.product);
+                  Future.microtask(() => Navigator.pop(context));
+                },
+              ),
+            )
+      ]
     );
 
   }
